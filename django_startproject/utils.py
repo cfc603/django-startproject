b6c19815132043ec614b34d81d0f1803a8e129a0
Trevor Watson <twatson603@gmail.com>
2016-01-29 18:29:49 -0500

    add secrets.json, update regex replace for SECRET_KEY

--- django_startproject/utils.py
+++ django_startproject/utils.py
@@ -58,7 +58,7 @@ def copy_template_file(src, dest, replace=None):
 
     # Generate SECRET_KEY for settings file
     secret_key = ''.join([choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)])
-    data = re.sub(r"(?<=SECRET_KEY = ')'", secret_key + "'", data)
+    data = re.sub(r'(?<="SECRET_KEY": ")"', secret_key + '"', data)
 
     # Write the data to the destination file.
     dest_file = open(dest, 'w')
