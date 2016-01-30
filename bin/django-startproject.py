6a6ad1622d3c6569e94ecee511a54d0989f63e85
Trevor Watson <twatson603@gmail.com>
2016-01-30 06:08:09 -0500

    update import

--- bin/django-startproject.py
+++ bin/django-startproject.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 def main():
-    from django_startproject.management import start_project
+    from django_startproject_template.management import start_project
     start_project()
 
 
