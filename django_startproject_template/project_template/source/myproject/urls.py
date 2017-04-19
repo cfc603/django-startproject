e81ac9dbb48a30fdc8c1f201908d158114f9b71d
cfc603 <wtrevor162@gmail.com>
2017-04-19 16:49:53 -0400

    set urls

--- django_startproject_template/project_template/source/myproject/urls.py
+++ django_startproject_template/project_template/source/myproject/urls.py
@@ -1,6 +1,18 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.static import serve
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('cms.urls')), # should be last
 ]
+
+# This is only needed when using runserver.
+if settings.DEBUG:
+    urlpatterns = [
+        url(r'^media/(?P<path>.*)$', serve,
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+        ] + staticfiles_urlpatterns() + urlpatterns
